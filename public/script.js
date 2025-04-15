ef2eb7022c44b333aacc92d0cae3fb5afe207ae8
Aarya Agrawal <aarya6105@gmail.com>
2025-04-15 19:39:07 +0530

    Added codes

--- public/script.js
+++ public/script.js
@@ -1,14 +1,18 @@
 var socket = io();
 
 let btn = document.getElementById('btn');
-btn.onclick= function(){
-        socket.emit('from_client');
-        
-}
-socket.on('from_server' , ()=>{
-    console.log('collected an event from the server')
-    const div = document.createElement('div');
-    div.innerText= 'new event from server';
-    document.body.appendChild(div);
+let inputmsg = document.getElementById('newmsg');
+let msgList  = document.getElementById('msglist');
 
+btn.onclick= function(){
+        socket.emit('msg_send',{
+            msg : inputmsg.value,
+        });
+
+}
+
+socket.on('msg_rcvd' , (data)=>{
+    let limsg = document.createElement('li');
+    limsg.innerText=data.msg;
+    msgList.appendChild(limsg);
 })
