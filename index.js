9a3753941288d8f287ca15933e2d46807d007328
Aarya Agrawal <aarya6105@gmail.com>
2025-04-16 00:44:41 +0530

    Added full chat app

--- index.js
+++ index.js
@@ -6,19 +6,55 @@ const { setInterval } = require('timers');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
+const {connect} = require('./config/database-config');
+const { console } = require('inspector/promises');
 
+const Chat= require('./models/chat')
 
 io.on('connection', (socket) => {
+    socket.on('join_room' ,(data)=>{
+
+        socket.join(data.roomid);
+    })
+    // socket.join()
     console.log('a user connected', socket.id);
 
-    socket.on('msg_send',(data)=>{
-        io.emit('msg_rcvd' , data)
+    socket.on('msg_send',async(data)=>{
+
+        const chat = await Chat.create({
+            roomId : data.roomid,
+            user : data.username,
+            content : data.msg
+        });
+        io.to(data.roomid).emit('msg_rcvd' , data)
+    })
+
+    socket.on('typing' , async(data)=>{
+        socket.broadcast.to(data.roomid).emit('someone_typing')
     })
 
 })
-
+app.set('view engine','ejs')
 app.use('/', express.static(__dirname + '/public'))
+app.get('/chat/:roomid',async(req,res)=>{
 
-server.listen(3000 , ()=>{
-    console.log("app is listening on the server 3000")
+    const chats = await Chat.find({
+        roomId:req.params.roomid
+    }
+
+    ).select('content user')
+    res.render('index' , {
+        name : 'Aarya',
+        id : req.params.roomid,
+        chats : chats
+    })
 })
+server.listen(3000, async () => {
+    console.log("app is listening on the server 3000");
+    try {
+        await connect();
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+    }
+});
